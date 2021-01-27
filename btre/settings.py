c336c838b73c36a8c720af56a3fd58d64dd382e5
Niklas <61664856+reanmoDev2@users.noreply.github.com>
2021-01-27 10:18:10 +0100

    setup mysql DB

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'btre_tut',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
